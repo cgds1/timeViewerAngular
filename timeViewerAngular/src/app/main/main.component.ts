c748aa1fa12035a3d56d12c43b87bac77a201883
Carlos Diaz <cg22ds1243@gmail.com>
2024-11-09 02:15:58 -0400

    Feat: Reloj de arena

--- timeViewerAngular/src/app/main/main.component.ts
+++ timeViewerAngular/src/app/main/main.component.ts
@@ -5,13 +5,14 @@ import { CommonModule } from '@angular/common';
 import { AnalogClockComponent } from '../analog-clock/analog-clock.component';
 import { DigitalClockComponent } from '../digital-clock/digital-clock.component';
 import { MilitaryClockComponent } from '../military-clock/military-clock.component';
+import { HourglassClockComponent } from '../hourglass-clock/hourglass-clock.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule, AnalogClockComponent, DigitalClockComponent, MilitaryClockComponent]
+  imports: [FormsModule, CommonModule, AnalogClockComponent, DigitalClockComponent, MilitaryClockComponent, HourglassClockComponent]
 })
 export class MainComponent {
   selectedView = ''; // Inicialmente vacío para que el reloj no se muestre
@@ -28,6 +29,8 @@ export class MainComponent {
         return 'Digital Clock';
       case 'military':
         return 'Military Clock';
+      case 'hourglass':
+        return 'Hourglass Clock';
       default:
         return '';
     }
